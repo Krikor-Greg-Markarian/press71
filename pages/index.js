0303f510971a6bc4497a9027a01989003a4e987c
rony <rriachi@bright-lab.com>
2021-09-14 16:14:10 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from "axios";
 
-export default function Home() {
+
+
+export default function Home(props) {
   return (
     <div>
-     
+      
     </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  const response = await axios.get("http://localhost:3000/api/posts");
+
+  return {
+    props: {
+      posts_data: response.data,
+    }, // will be passed to the page component as props
+  };
+}
+
+
+
+
+
