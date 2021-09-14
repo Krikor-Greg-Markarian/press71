3371052b96795b8d99c9553a052b2935caeadff8
rony <rriachi@bright-lab.com>
2021-09-14 17:10:19 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import axios from "axios";
+import Navbar from './Navbar';
 
 
 
 export default function Home(props) {
   return (
     <div>
-      
+      <Navbar />
     </div>
   )
 }
