8b065e3889fe06fca45886a767c49dcb600159c5
rony <rriachi@bright-lab.com>
2021-09-14 16:14:21 +0300

    JSON

---
+++ pages/api/posts.js
@@ -0,0 +1,115 @@
+
+export default function handler(req, res) {
+  const object = {
+    data: [
+      {
+        coronabutton:"CORONAVIRUS",
+        coronatitle:"Coronavirus live updates:italy death toll nears 3000",
+        coronatitle2:"Virus confronts leaders with one of modern history's gravest challenges",
+        livebutton:"LIVE",
+        livebutton2:"34 MINUTE AGO",
+        why:"Why Covid-19 is worse than the flu, in one chart shit",
+        asupermarket:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the virus and die), as panic-buying blighed the nations...",
+        recentnews:"RECENT NEWS",
+        rasalina:"BY: EASALINA",
+        ibuprofen:"ibuprofen should not be used for managing symptoms.say doctors and scientists",
+        exclusive:"Exlusive: Amazon Confirms First Known Coronavirus Case in an Ameican Warehouse",
+        trump:"Trump sought to buy vaccine developer exclusively for US, say German officials",
+        coronavirus:"Coronavirus live updates:italy death toll nears 3,000",
+      },
+      {
+        
+        advicestories: "STORIES",
+        advicecoronavirus:"Coronavirus live update: Total US cases near 9,000 as death toll approaches 150",
+        adviceadvice: "ADVICE",
+        advicethehealthiest:"The Healthiest Way to Sweat Out a pandemic",
+        advicehospital:"HOSPITAL",
+        advicewhattodo:"What to Do About a Light Case of COVID-19",
+        absolutetext:"Virus confronts leaders with one of modern histor's gravest challenges",
+        virusupdate:"VIRUS UPDATE",
+        colorodo:"Colorodo Travel Guide: What to See,Do,Costs,Roads",
+        pandamic:"PANDAMIC",
+        van:"Van trip: 3 Days in San Sebastian, Spain",
+        medicine:"MEDICINE",
+        independent:"Independent Trekking and Camping in Nepal",
+        localnews:"LOCAL NEWS",
+        peru:"Peru Travel Fuide: What to See,Do,Costs,Roads",
+
+      },
+
+      {
+        doctorbutton:"DOCTOR",
+        forteencoronavirus:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+        quarantinestext:"Quarantines spark Confusion and Worries About Precedent",
+        theretext:"There Aren't Enough Ventilators to Cope With the Coronavirus",
+        acalltext:"A Call to Arms: Under Attack, Pro-Vaccine Doctors Fight Back",
+        latestbuttoncheckpoint:"CHECK POST",
+        workbutton:"WORK"
+      },
+
+      {
+        globaltext:"Global research and innovation forum: towards a research roadmap",
+        forthose:"for those of us who wants to say thank you to our moms, it's not always easy to put those big feelings in words. which is where dribble comes in.",
+        theseeight:"These eight shots crystallize the hard work moms put into keeping their kids allive, happy, and healthy. They might give you the inpiration you need for filling out that card--or stand alone for your mom's interpretation.",
+        momsare:"Moms are the oens who bandage our boo-boos when we're little and continue to take care of us as we get older-often sacrificing their own needs so they can help with ours. Cruising on a bike"
+      },
+
+      {
+        herecome:"Here come the moms in space",
+        aneyewitness:"An eyewitness described the scene at a packed branch of Asda in the Wirral,Merseyside, on saturday, as a man in his 30s attemped to buy more than the three Pot Noodles allowed by the store.",
+       
+      },
+      {
+        wrapped:"Wrapped up in making sure everybody",
+        theincident:"The incident, which was rasied in palimant by Labour MP Bill Esterson, was just one of the horrendous cases of abuse revealed by supermarket wokers in recent days as some customers grow angry over restrictions and empty shelves.",
+        thedoctor:"The doctor was exposed to the virus when the 38-year-old woman visited the clinic on March 12, the minister said. Five days later, she testes positive. That day, the doctor was also admitted in hospital. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure enim minima ducimus blanditiis consectetur id maiores nisi vel fugit illum, exercitationem ullam laudantium. Reprehenderit quis repellat cum porro magni voluptate.",
+        duis:"Duis aute irune dolor in reprehenderit in voluptate velit esse cillium dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit",
+      },
+      {
+        unordered:"Unordered list style",
+        momsarelike:"Moms are like...buttons? Moms are like glue. Moms are like pizza vrusts. Moms are the ones who make sure things happen-from birth to school lunch.",
+        theyarethe:"They're the ones we rely on for playdates and emotional support,homework help and babysitting. Moms are the ultimate dependable support. Like, hopefully, the button on your jeans.",
+        myhero:"My hero when i was a kid was my mom. Same for everyone i knew. Moms are untouchable. They're elegant,smart,beautiful,kind...everything we want to be. At 29 years old, my favorite compliment i sbeing told that i look like my mom. Seeing myself in her image, like this daughter up top, make me so proud of how far I've come, and so thankful for where i come from."
+      },
+
+      {
+        releteddate:"24th March 2019",
+        watch:"Watch:10 Days Later: What italians Wish They Had known",
+        asupermarketsmall:"A supermarket worker wasw spat at by a customer to stockpile.."
+
+      },
+
+      {
+        writtenby:"Written by",
+        rosalinda:"Rosalinda D.William",
+        stephen:"tephen Romero has worked a italy report since its launch in 2011. In the years since, he has led by example the company's tech news team and published over 5,000 articles..."
+      },
+
+      {
+        rosalinakelian:"Rosalina Kelian",
+        commentslorem:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas id fuga corrupti veritatis libero commodi impedit quo dolores quidem alias, est consequatur sit in quae. Quaerat exercitationem dolor.",
+        commentslorem2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas id fuga corrupti veritatis.",
+        commentslorem3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas id fuga corrupti veritatis libero commodi impedit quo dolores quidem alias, est consequatur sit in."
+      },
+
+      {
+        rasalinda:"Rasalinada De Willamson",
+        articelswritten:"64 Articles Written",
+        numberofcomments:"2Comments",
+        Rasalinadehas:"Rasalina De has worked a italy report since its launch in 2011. In the years since, he has led by example the company's tech news team and published over 5,000 articles - a mix of breaking news,reviews,helpful how-tos, industry analysis, and more.n Romero has worked a Italy Report since its launch in 2011. In the years since."
+      },
+
+      {
+        allpostpenn:"penn's expanding political climate gears up for 2020 election",
+        allpostthings:"Things to look For in a Financial Trading platform",
+        allpostbeautiful:"Beautiful waterfalls that will take your breath away",
+        allpostasupermarket:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...)"
+      },
+    ]
+  }
+  res.status(200).json(object)
+}
+
+
+
+
