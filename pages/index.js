7c00e46442b2f65582558df93cdde3e7956ec205
rony <rriachi@bright-lab.com>
2021-09-14 17:37:58 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,51 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import axios from "axios";
 import Navbar from './Navbar';
+import Navbar2 from './api/Navbar2';
 
 
 
 export default function Home(props) {
   return (
+
+
     <div>
+      <section>
       <Navbar />
+      </section>
+      <section>
+        <Navbar2 />
+      </section>
+
+      <section className = "container mx-auto pt-12">
+        <button className = "bg-red-600 text-white text-sm font-bold px-4 py-1 inline">TRADING NOW</button>
+        <p className = "inline ml-5 font-bold text-sm text-gray-600">Trump sough to buy vaccine developer exclusively for US, say German officials updates</p>
+      </section>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   )
 }
 
