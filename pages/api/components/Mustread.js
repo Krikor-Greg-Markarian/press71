a9b415991d08b974c99d3da139573668d013367e
rony <rriachi@bright-lab.com>
2021-09-15 17:52:57 +0300

    must read

---
+++ pages/api/components/Mustread.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Mustread() {
+    return (
+        <div>
+            <div className="pt-2 pb-2">
+                      <p className="text-gray-400 inline text-sm">
+                        24 DECEMBER 2021{" "}
+                        <span className="text-gray-400 inline text-sm">
+                          {" "}
+                          <span className="text-black font-bold ">
+                            BY :
+                          </span>{" "}
+                          RASALINA
+                        </span>{" "}
+                      </p>
+                      <p className="font-bold text-sm pt-2 pb-2">
+                        Coronavirus live updates:italy death toll nears 3,000
+                      </p>
+                      <div className="border-b-2"></div>
+                    </div>
+        </div>
+    )
+}
+
+export default Mustread
