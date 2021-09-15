eab6980ac06bb31dced03f582956ac5066865337
rony <rriachi@bright-lab.com>
2021-09-15 13:18:15 +0300

    img text

---
+++ pages/api/components/Adviceleft.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Adviceleft(props) {
+    return (
+        <div>
+            <div className = "design "></div>
+            <div className = "pt-2">
+            <button className = "bg-red-500 text0white px-4 ">CORONAVIRUS</button>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, iure itaque. </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ab harum ipsam reprehenderit at natus eligendi autem excepturi magnam tenetur. Voluptate neque dolores qui odit reiciendis placeat praesentium nobis similique!</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Adviceleft
