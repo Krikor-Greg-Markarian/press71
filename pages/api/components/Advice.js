bde2b7fbc2dfb24b7f5182ee549c6f5f94e2d241
rony <rriachi@bright-lab.com>
2021-09-15 17:52:33 +0300

    advice section

--- pages/api/components/Advice.js
+++ pages/api/components/Advice.js
@@ -1,59 +1,18 @@
 import React from 'react'
 
-function Advice() {
+function Advice(props) {
     return (
-        <div className="grid grid-cols-3 container mx-auto">
-        <div className="col-span-1">
-          <div className="">
-            <p>stories</p>
-            <p>stories</p>
-            <p className="inline">stories</p>
-            <p className="inline"> stories</p>
-            <div className="pt-2">
-              <p>stories</p>
-              <p>stories</p>
-              <p className="inline">stories</p>
-              <p className="inline"> stories</p>
+        <div>
+            <div className = "container mx-auto">
+            <div className = "grid grid-cols-3 ">
+                <div className = " col-span-1">
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
             </div>
-            <div className="pt-2">
-              <p>stories</p>
-              <p>stories</p>
-              <p className="inline">stories</p>
-              <p className="inline"> stories</p>
             </div>
-          </div>
         </div>
-        <div className="com-span-1">img</div>
-        <div className="com-span-1">
-          <div class="flex">
-            <div class="flex-shrink w-16 h-16 ...">
-              <p>virusupdate</p>
-              colorado
-            </div>
-            <div class="flex-shrink w-80 h-16 ...">
-              <div className="small"></div>
-            </div>
-          </div>
-          <div class="flex">
-            <div class="flex-shrink w-16 h-16 ...">
-              <p>virusupdate</p>
-              colorado
-            </div>
-            <div class="flex-shrink w-80 h-16 ...">
-              <div className="small"></div>
-            </div>
-          </div>
-          <div class="flex">
-            <div class="flex-shrink w-16 h-16 ...">
-              <p>virusupdate</p>
-              colorado
-            </div>
-            <div class="flex-shrink w-80 h-16 ...">
-              <div className="small"></div>
-            </div>
-          </div>
-        </div>
-      </div>
     )
 }
 
