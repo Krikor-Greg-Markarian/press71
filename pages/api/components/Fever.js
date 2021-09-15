17e63e3dc921953139659b3ca82e4be4efcb58ce
rony <rriachi@bright-lab.com>
2021-09-15 17:52:52 +0300

    fever section

---
+++ pages/api/components/Fever.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Fever(props) {
+  return (
+    <div>
+      <div className="">
+        <div className="design"></div>
+        <div className = "pt-2">
+        <button className="bg-red-600 text-white px-6 ">coronoasdas</button>
+        </div>
+        <p className = "font-bold">why cvovid19</p>
+        <p className = "text-gray-400 text-sm">Loremasdasdasdasdasdasdasdasdasdsadsad</p>
+      </div>
+    </div>
+  );
+}
+
+export default Fever;
