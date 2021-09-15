3bc9c953e5b8333e4d2dac73acb442a57a429a79
rony <rriachi@bright-lab.com>
2021-09-15 13:18:30 +0300

    img text info

---
+++ pages/api/components/Latestarticle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Latestarticle() {
+    return (
+        <div className="container mx-auto">
+        <div className="grid grid-cols-4">
+          <div className="col-span-1">
+            <div className="design2"></div>
+            <p>140 corona</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              laborum exercitationem asperiores doloribus impedit, nostrum
+              alias tempora sunt nulla, repudiandae iste cumque quam odit
+              ipsum assumenda culpa! Totam, voluptatibus eius!
+            </p>
+            <p className="inline">by:rasalina</p>
+            <p className="inline">23 comments</p>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Latestarticle
