8bfbb9e18e903a353e7e942ada281f6574cd0013
rony <rriachi@bright-lab.com>
2021-09-16 17:48:41 +0300

    small advice component

---
+++ pages/api/components/Smalladvice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Smalladvice() {
+    return (
+        <div>
+            <div>
+                <p className = "text-gray-500 text-sm">STORIES</p>
+                <p className = "text-sm pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet exercitationem facere laborum voluptatum eum itaque.</p>
+                <p className = "text-gray-500 text-sm pt-2">34 MINUTES AGO <p className = "text-black text-sm inline">By: <span className = "text-blue-600 text-sm inline">RASALINA</span> </p>  </p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Smalladvice
