2384d0d7d0e4fa8ac01ad67015ff44580501b817
rony <rriachi@bright-lab.com>
2021-09-17 16:45:24 +0300

    virus update section

--- pages/api/components/Virusupdate.js
+++ pages/api/components/Virusupdate.js
@@ -3,20 +3,22 @@ import React from "react";
 function Virusupdate() {
   return (
     
-      <div>
-        <div class="flex">
-          <div class="flex-grow">
+      <div className = "">
+        <div class="flex ">
+          
+          <div class="flex-grow ">
             <p className="text-gray-400 text-sm pl-6">VIRUS UPDATE</p>
             <p className = "pl-6 pr-1">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
               nobis.
             </p>
           </div>
-          <div class="flex-shrink">
+          <div class="flex-shrink pt-2 pl-1">
             <div className="small"></div>
           </div>
         </div>
-      </div>
+        </div>
+      
     
   );
 }
