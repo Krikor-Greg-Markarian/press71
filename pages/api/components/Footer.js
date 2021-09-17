1fa7fc6941bfc50dc6663f56376eac8656d32a22
rony <rriachi@bright-lab.com>
2021-09-17 12:01:11 +0300

    footer

--- pages/api/components/Footer.js
+++ pages/api/components/Footer.js
@@ -10,114 +10,15 @@ import {
 
 function Footer() {
   return (
-    // <div className="bg-black">
-    //   <div className="p-20">
-    //     <div className="grid grid-cols-4">
-    //       <div className="col-span-1 ">
-    //         <div className="text-center ">
-    //           <p className="inline font-bold text-white">PRESS</p>
-    //           <p className="inline text-red-400 font-bold">71</p>
-
-    //           <div>
-    //             <FaTwitter className="text-white inline" />
-    //             <FaFacebook className="text-white inline ml-3" />
-    //             <FaYoutube className="text-white inline ml-3" />
-    //             <FaGooglePlusG className="text-white inline ml-3" />
-    //             <FaInstagram className="text-white inline ml-3" />
-    //           </div>
-    //         </div>
-    //       </div>
-
-    //       <div className="col-span-1">
-    //         <p className="text-white">RECENT POSTS</p>
-    //         <div class="flex">
-    //           <div class="flex-grow w-16 h-16">
-    //             <div className="small2"></div>
-    //           </div>
-    //           <div class="flex-shrink w-96 h-16 ...">
-    //             <p className="text-white text-sm">
-    //               {" "}
-    //               Health This is What physical Therapy Can Actually Solve
-    //             </p>
-    //           </div>
-    //         </div>
-    //         <div class="flex pt-4">
-    //           <div class="flex-grow w-16 h-16">
-    //             <div className="small2"></div>
-    //           </div>
-    //           <div class="flex-shrink w-96 h-16 ...">
-    //             <p className="text-white text-sm">
-    //               {" "}
-    //               Health This is What physical Therapy Can Actually Solve
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>
-
-    //       <div className="col-span-1 ">
-    //         <div className="">
-    //           <p className="text-white">POPULAR CATEGORIES</p>
-
-    //           <p className="text-gray-400 text-sm">
-    //             Stories{" "}
-    //             <span className="text-sm text-gray-400 float-right">23</span>{" "}
-    //           </p>
-    //           <p className="text-gray-400 text-sm">
-    //             Health{" "}
-    //             <span className="text-sm text-gray-400 float-right">45</span>{" "}
-    //           </p>
-    //           <p className="text-gray-400 text-sm">
-    //             Local news{" "}
-    //             <span className="text-sm text-gray-400 float-right">67</span>{" "}
-    //           </p>
-    //           <p className="text-gray-400 text-sm">
-    //             Video{" "}
-    //             <span className="text-sm text-gray-400 float-right">98</span>{" "}
-    //           </p>
-    //           <p className="text-gray-400 text-sm">
-    //             Education{" "}
-    //             <span className="text-sm text-gray-400 float-right">23</span>{" "}
-    //           </p>
-    //           <p className="text-gray-400 text-sm">
-    //             Coronavirus{" "}
-    //             <span className="text-sm text-gray-400 float-right">56</span>{" "}
-    //           </p>
-    //         </div>
-    //       </div>
-    //       <div className="col-span-1 ">
-    //         <div className=" pl-12">
-    //           <p className="text-white">TWITTER FEEDS</p>
-    //           <p className="text-sm text-gray-400">
-    //             carsafe-#Gutenbergts,Car Dealer available on @website{" "}
-    //             <span className="text-white"> https://website.net</span>
-    //           </p>
-    //           <p>june 22,2021</p>
-    //         </div>
-    //         <div className=" pl-12">
-    //           <p className="text-white">TWITTER FEEDS</p>
-    //           <p className="text-sm text-gray-400">
-    //             carsafe-#Gutenbergts,Car Dealer available on @website{" "}
-    //             <span className="text-white"> https://website.net</span>
-    //           </p>
-    //           <p>june 22,2021</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="flex justify-center items-center border-t-2 p-6">
-    //     <p className="text-sm text-white">
-    //       News71 &copy; 2021. Allrights reserved
-    //     </p>
-    //   </div>
-    // </div>
-
     <div className="pt-8">
       <div className="bg-black ">
         <div className="grid grid-cols-4">
           <div className="col-span-1 pt-16 border-r-2 border-b-2">
             <div className="flex justify-center items-center">
-              <p className="font-bold text-white ">
-                PRESS <span className="text-red-500">71</span>{" "}
+              <div className="">
+                <p className="font-bold text-white text-2xl">
+                  PRESS <span className="text-red-500">71</span>{" "}
+                </p>
                 <div className="pt-4">
                   <FaTwitter className="text-white inline" />
                   <FaFacebook className="text-white inline ml-3" />
@@ -125,7 +26,7 @@ function Footer() {
                   <FaGooglePlusG className="text-white inline ml-3" />
                   <FaInstagram className="text-white inline ml-3" />
                 </div>{" "}
-              </p>
+              </div>
             </div>
           </div>
           <div className="col-span-1 border-r-2 border-b-2 p-4">
