40a81a98f5dce013dc95056398409219ce6860be
rony <rriachi@bright-lab.com>
2021-09-17 16:44:45 +0300

    must read section

--- pages/api/components/Mustread.js
+++ pages/api/components/Mustread.js
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Mustread() {
-    return (
-        <div>
-            <div className="pt-2 pb-2">
-                      <p className="text-gray-400 inline text-sm">
-                        24 DECEMBER 2021{" "}
-                        <span className="text-gray-400 inline text-sm">
-                          {" "}
-                          <span className="text-black font-bold ">
-                            BY :
-                          </span>{" "}
-                          RASALINA
-                        </span>{" "}
-                      </p>
-                      <p className="font-bold text-sm pt-2 pb-2">
-                        Coronavirus live updates:italy death toll nears 3,000
-                      </p>
-                      <div className="border-b-2"></div>
-                    </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="pt-2 pb-2">
+        <p className="text-gray-400 inline text-sm">
+          24 DECEMBER 2021{" "}
+          <span className="text-gray-400 inline text-sm">
+            <span className="text-black font-bold text-sm pl-4 ">BY :</span> RASALINA
+          </span>
+        </p>
+        <p className="font-bold text-sm pt-2 pb-2">
+          Coronavirus live updates:italy death toll nears 3,000
+        </p>
+        <div className="border-b-2"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Mustread
+export default Mustread;
