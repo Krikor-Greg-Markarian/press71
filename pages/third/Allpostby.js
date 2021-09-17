d6419bbfe0eafef4ba7da73e63012e818d456d00
rony <rriachi@bright-lab.com>
2021-09-17 11:45:25 +0300

    all post last page

--- pages/third/Allpostby.js
+++ pages/third/Allpostby.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Allpostby() {
-    return (
-        <div className = "grid grid-cols-4">
-            <div className = "col-span-1">
-           <div className = "holiday"></div>
-           <p className = "font-bold text-sm pt-2">14 Coronavirus myth busted by WHO for better covid-19 awareness</p>
-           <p className = "text-gray-400 text-sm pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, dolorem repellat quia vero aspernatur voluptatum error.</p>
-           </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="holiday mb-2 relative">
+        <button className = "text-white bg-red-500 px-2 absolute bottom-0">Doctor</button>
+      </div>
+      <p className="font-bold text-sm pt-2">
+        14 Coronavirus myth busted by WHO for better covid-19 awareness
+      </p>
+      <p className="text-gray-400 text-sm pt-2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore,
+        dolorem repellat quia vero aspernatur voluptatum error.
+      </p>
+      <p className="font-bold text-sm inline ">
+        BY:{" "}
+        <span className="text-blue-600 text-sm font-bold inline ">RASALINA</span>{" "}
+      </p>
+      <p className="inline">
+        | <span className="text-sm text-gray-400">23comments</span>{" "}
+      </p>
+    </div>
+  );
 }
 
-export default Allpostby
+export default Allpostby;
