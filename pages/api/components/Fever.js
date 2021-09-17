9cd6dfbd274f6707ddae1dab92e7c561621b6300
rony <rriachi@bright-lab.com>
2021-09-17 16:44:22 +0300

    fever component

--- pages/api/components/Fever.js
+++ pages/api/components/Fever.js
@@ -6,10 +6,10 @@ function Fever(props) {
       <div className="">
         <div className="design"></div>
         <div className = "pt-2">
-        <button className="bg-red-600 text-white px-6 ">coronoasdas</button>
+        <button className="bg-red-600 text-white px-6 ">Coronavirus</button>
         </div>
-        <p className = "font-bold">why cvovid19</p>
-        <p className = "text-gray-400 text-sm">Loremasdasdasdasdasdasdasdasdasdsadsad</p>
+        <p className = "font-bold pt-2 pb-2">why covid-19 is worse than the flue, in one chart shit</p>
+        <p className = "text-gray-400 text-sm sm:mb-4">A Supermarket worker was spat at by a customer attempting to stockpile pot noodles while another was told; "i hope you geet the virus and die", as panic-buying blighted the nations...</p>
       </div>
     </div>
   );
