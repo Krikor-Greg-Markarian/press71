2976c031cccf75dc815eb8444dc453323e08d2a5
rony <rriachi@bright-lab.com>
2021-09-17 16:44:37 +0300

    latest article section

--- pages/api/components/Latestarticle.js
+++ pages/api/components/Latestarticle.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 function Latestarticle() {
     return (
-        <div className="container mx-auto">
-        <div className="grid grid-cols-4">
-          <div className="col-span-1">
-            <div className="design2"></div>
-            <p>140 corona</p>
-            <p>
+       
+       
+          <div className = "sm:mb-4 ">
+            <div className="design2 w-full relative">
+              <button className = "text-white bg-red-600 px-2 text-sm absolute bottom-0">DOCTOR</button>
+            </div>
+            <p className = "font-bold text-sm pt-4">14 coronavirus myths busted by WHO fo better Covid-19 awareness</p>
+            <p className = "text-sm text-gray-400 pt-4 pb-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              laborum exercitationem asperiores doloribus impedit, nostrum
-              alias tempora sunt nulla, repudiandae iste cumque quam odit
-              ipsum assumenda culpa! Totam, voluptatibus eius!
+              laborum exercitationem asperiores doloribus impedit.
             </p>
-            <p className="inline">by:rasalina</p>
-            <p className="inline">23 comments</p>
+            <p className="inline font-bold text-sm">By: <span className = "text-blue-500 text-sm inline font-bold">RASALINA</span> </p>
+            <p className="inline text-gray-400 text-sm">| 23 comments</p>
           </div>
-        </div>
-      </div>
+        
+      
     )
 }
 
