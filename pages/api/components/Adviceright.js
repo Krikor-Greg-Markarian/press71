3abfe174ad4356cd88c9c18bb33aaaa40e44653b
rony <rriachi@bright-lab.com>
2021-09-17 17:42:55 +0300

    advertise right

--- pages/api/components/Adviceright.js
+++ pages/api/components/Adviceright.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Adviceright() {
   return (
-    <div class="flex mb-4">
-      <div class="flex-grow w-16 h-16">
+    <div class="flex">
+      <div class="flex-grow ">
         <div className="small"></div>
       </div>
-      <div class="flex-shrink w-96 h-16 ">
-        <p className="inline text-gray-400 text-sm ">RECENT NEWS</p>
+      <div class="flex-shrink">
+      <p className="inline text-gray-400 text-sm ">RECENT NEWS</p>
         <p className="inline ml-2">
           By: <span className="inline text-blue-500 text-sm">RASALINA</span>{" "}
         </p>
