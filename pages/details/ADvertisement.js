40603bd11e27a1cfce5f05274715f4aed16f39cf
rony <rriachi@bright-lab.com>
2021-09-20 15:43:55 +0300

    advertisement section

---
+++ pages/details/ADvertisement.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ADvertisement() {
+    return (
+        <div>
+            <div className="container mx-auto p-4">
+                    <p className="font-bold ">Advertisement</p>
+
+                    <div className="flex justify-center items-center pt-4">
+                      <div className="astronoid"></div>
+                    </div>
+                  </div>
+        </div>
+    )
+}
+
+export default ADvertisement
