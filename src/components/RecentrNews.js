2e242ed07226b957c1a37702fc0bd01677b269df
rony <rriachi@bright-lab.com>
2021-09-20 10:11:59 +0300

    recenet news section

--- src/components/RecentrNews.js
+++ src/components/RecentrNews.js
@@ -5,7 +5,13 @@ function RecentrNews() {
        
         <div className="flex pt-1">
         <div className="flex-grow ">
-          <div className="small"></div>
+          <div className="small relative">
+            <div className = "red rounded-full text-white absolute -right-2 top-6">
+              <div className = "flex justify-center items-center">
+              <p className = "text-center text-sm">01</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="flex-shrink ">
           <p className=" pl-4 inline text-gray-400 text-sm ">RECENT NEWS</p>
