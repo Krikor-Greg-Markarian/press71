42940eb55f593ed453ec3dc0963eb656dad31371
rony <rriachi@bright-lab.com>
2021-09-20 15:45:13 +0300

    recent new

--- src/components/RecentrNews.js
+++ src/components/RecentrNews.js
@@ -1,28 +1,26 @@
 import React from 'react'
 
-function RecentrNews() {
+function RecentrNews(props) {
     return (
        
-        <div className="flex pt-1">
-        <div className="flex-grow ">
+        <div className="flex">
+        <div className="flex-grow pb-2">
           <div className="small relative">
-            <div className = "red rounded-full text-white absolute -right-2 top-6">
+            <div className = "red rounded-full text-white absolute -right-2 top-7">
               <div className = "flex justify-center items-center">
               <p className = "text-center text-sm">01</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex-shrink ">
-          <p className=" pl-4 inline text-gray-400 text-sm ">RECENT NEWS</p>
+        <div className="flex-shrink">
+          <p className=" pl-4 inline text-gray-400 text-sm ">{props.recentnews}</p>
           <p className="inline ml-2">
             By: <span className="inline text-blue-500 text-sm">RASALINA</span>
           </p>
           <p className="text-sm pl-4">
-            Lorem, ipsum dolor sit amet sectetur adipisicing elit. Temporibus
-            dolore, aspernatur.
+           {props.ibuprofen}
           </p>
-          <p className="text-sm"></p>
         </div>
       </div>
       
