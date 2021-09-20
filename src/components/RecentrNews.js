f0c40b5f3a79a4cde4c16db6635c5810c57ed21d
rony <rriachi@bright-lab.com>
2021-09-20 09:52:19 +0300

    recent news section

---
+++ src/components/RecentrNews.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function RecentrNews() {
+    return (
+       
+        <div className="flex pt-1">
+        <div className="flex-grow ">
+          <div className="small"></div>
+        </div>
+        <div className="flex-shrink ">
+          <p className=" pl-4 inline text-gray-400 text-sm ">RECENT NEWS</p>
+          <p className="inline ml-2">
+            By: <span className="inline text-blue-500 text-sm">RASALINA</span>
+          </p>
+          <p className="text-sm pl-4">
+            Lorem, ipsum dolor sit amet sectetur adipisicing elit. Temporibus
+            dolore, aspernatur.
+          </p>
+          <p className="text-sm"></p>
+        </div>
+      </div>
+      
+    )
+}
+
+export default RecentrNews
