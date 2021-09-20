2bac47849e59a2cf54593674fb430c3fb2171a38
rony <rriachi@bright-lab.com>
2021-09-20 09:52:37 +0300

    advice right section

--- src/components/Adviceright.js
+++ src/components/Adviceright.js
@@ -3,7 +3,7 @@ import React from "react";
 const Adviceright = (props) => {
   return (
     <div className="flex">
-      <div className="flex-grow ">
+      <div className="flex-grow">
         <div className="small"></div>
       </div>
       <div className="flex-shrink">
@@ -11,7 +11,7 @@ const Adviceright = (props) => {
         <p className="inline ml-2">
           By: <span className="inline text-blue-500 text-sm">RASALINA</span>
         </p>
-        <p className="text-sm ">
+        <p className="text-sm">
           Lorem, ipsum dolor sit amet sectetur adipisicing elit. Temporibus
           dolore, aspernatur.
         </p>
