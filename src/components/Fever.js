f092119f9a25fa03c3b9e9b4515a59be30a0ad29
rony <rriachi@bright-lab.com>
2021-09-20 15:43:36 +0300

    fever section

--- src/components/Fever.js
+++ src/components/Fever.js
@@ -4,12 +4,12 @@ function Fever(props) {
   return (
     
       <div className = "">
-        <div className="design"></div>
+        <div className="design w-full"></div>
         <div className = "pt-2">
-        <button className="bg-red-600 text-white px-6 ">Coronavirus</button>
+        <button className="bg-red-600 text-white px-6 ">{props.coronabutton}</button>
         </div>
-        <p className = "font-bold pt-2 pb-2">why covid-19 is worse than the flue, in one chart shit</p>
-        <p className = "text-gray-400 text-sm sm:mb-4">A Supermarket worker was spat at by a customer attempting to stockpile pot noodles while another was told; "i hope you geet the virus and die", as panic-buying blighted the nations...</p>
+        <p className = "font-bold pt-2 pb-2">{props.why}</p>
+        <p className = "text-gray-400 text-sm sm:mb-4">{props.asupermarket}</p>
       </div>
     
   );
