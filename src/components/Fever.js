5436bd47c2a0e33bb6835acb561bd94b0bcc5a63
rony <rriachi@bright-lab.com>
2021-09-20 09:52:07 +0300

    fever section

--- src/components/Fever.js
+++ src/components/Fever.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Fever(props) {
   return (
-    <div>
-      <div className="">
+    
+      <div className = "">
         <div className="design"></div>
         <div className = "pt-2">
         <button className="bg-red-600 text-white px-6 ">Coronavirus</button>
@@ -11,7 +11,7 @@ function Fever(props) {
         <p className = "font-bold pt-2 pb-2">why covid-19 is worse than the flue, in one chart shit</p>
         <p className = "text-gray-400 text-sm sm:mb-4">A Supermarket worker was spat at by a customer attempting to stockpile pot noodles while another was told; "i hope you geet the virus and die", as panic-buying blighted the nations...</p>
       </div>
-    </div>
+    
   );
 }
 
