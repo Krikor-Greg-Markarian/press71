73f1bdc09296f3bc303cdff3a1b7a5f9d9e6da7d
Greg <greg_markarian@hotmail.com>
2021-09-21 10:46:20 +0300

    ADD: Props

--- pages/details/index.js
+++ pages/details/index.js
@@ -6,6 +6,7 @@ import {
   FaInstagram,
   FaTwitter,
 } from "react-icons/fa";
+import axios from "axios";
 import Navbar2 from "../../src/components/Navbar2";
 import ImgText from "./imgtext";
 import Comments from "./Comments";
@@ -15,7 +16,7 @@ import Socialconnect from "./Socialconnect";
 import Link from "next/link";
 import Navbar from "./Navbar";
 
-function Secondpage() {
+export default function Secondpage(props) {
   return (
     <div>
       <div className="container mx-auto">
@@ -49,7 +50,7 @@ function Secondpage() {
               Global research and innovation forum: toward a reasearch rodmap
             </p>
           </ul>
-          <p className="font-bold text-2xl">
+          <p className="font-bold text-2xl pt-4 pb-4">
             Global research and innovation forum: towards <br></br> a research
             roadmap
           </p>
@@ -57,66 +58,90 @@ function Secondpage() {
       </div>
 
       <div className="grid grid-cols-2 container mx-auto">
-        <div className="col-span-1">
+        <div className="col-span-1 ">
           <div>
-            <p className="text-black inline">
-              BY:{" "}
-              <span className="text-blue-600 font-bold text-sm inline ml-2">
-                RASALINA
-              </span>{" "}
+            <div className="pt-4 pb-4">
+              <p className="text-black inline">
+                BY:{" "}
+                <span className="text-blue-600 font-bold text-sm inline ml-2">
+                  RASALINA
+                </span>{" "}
+              </p>
+              <p className="text-gray-400 text-sm font-bold inline ml-3">
+                Health Tips
+              </p>
+              <p className="text-gray-400 text-sm font-bold inline ml-3 ">
+                30 Commnets
+              </p>
+              <p className="text-gray-400 text-sm font-bold inline ml-3">
+                04thMarch2021
+              </p>
+            </div>
+            <p className="text-sm text-gray-400 pt-2 pb-2">
+              {props.paragraph.title}
             </p>
-            <p className="text-gray-400 text-sm font-bold inline ml-3">
-              Health Tips
-            </p>
-            <p className="text-gray-400 text-sm font-bold inline ml-3">
-              30 Commnets
-            </p>
-            <p className="text-gray-400 text-sm font-bold inline ml-3">
-              04thMarch2021
-            </p>
-            <p className="text-sm text-gray-400 pt-2">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
-              doloribus est rerum, perspiciatis aliquid labore non qui autem
-              repellat nostrum unde dolor maxime. Vitae voluptatibus rem
-              assumenda saepe consequuntur?
-            </p>
-            <p className="text-sm text-gray-400 pt-2">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
-              doloritem repellat nostrum unde dolor maxime. Vitae voluptatibus
-              rem assumenda saepe consequuntur?
+            <p className="text-sm text-gray-400 pt-2 pb-2">
+              {props.paragraph.subtitle}
             </p>
             <p className="text-sm text-gray-400 pt-2">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
-              dolorme. Vitae vptatibus rem assumenda saepe consequuntur?
+              {props.paragraph.subsubtitle}
             </p>
           </div>
-          <p className="font-bold pt-4">Here come the moms in space</p>
-          <p className="text-sm text-gray-400 pt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. repellat
-            nrum unde dolor maxime. Vitae voluptatibus rem assumenda saepe
-            consequuntur?
+          <p className="font-bold pt-4 text-lg">Here come the moms in space</p>
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+            {props.hereCome.asupermarket}
+          </p>
+
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+            {props.hereCome.aneyewitness}
+          </p>
+
+          <section className="pb-2">
+            <div className=" pt-3 pb-8 w-screen">
+              <div className="flex justify-start">
+                <div className="gift2 relative">
+                  <div className="container mx-auto">
+                    <div className="absolute transform translate-y-8 translate-x-6">
+                      <p className="text-white font-bold ">
+                        PRESS<span className="text-red-600">71</span>{" "}
+                      </p>
+
+                      <p className="text-white font-bold text-sm">
+                        Blog &amp; MagazineTheme
+                      </p>
+                      <div>
+                        <button className="bg-red-600 text-white text-sm px-3  transform -translate-y-12 translate-x-32 rounded-full">
+                          Buy Now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <p className="font-bold text-lg">
+            wrapped up in making sure everybody
+          </p>
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+           {props.wrapped.article}
+          </p>
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+            {props.wrapped.subarticle}
+          </p>
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+           {props.wrapped.subsubarticle}
+          </p>
+          <p className="font-bold pt-4 pb-2 text-lg">Unordered list style ?</p>
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+            {props.unordered.unorderedtitle}
+          </p>
+          <p className="text-sm text-gray-400 pt-2 pb-2">
+            {props.unordered.unorderedsubtittle}
           </p>
           <p className="text-sm text-gray-400 pt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel lat
-            nostrum unde dolor maxime. Vitae voluptatibus rem assumenda saepe
-            consequuntur?
-          </p>
-          <p className="text-sm text-gray-400 pt-2">
-            Lorem ipsum dolor sit, amuntur?
-          </p>
-          <p className="font-bold pt-4">Unordered list style ?</p>
-          <p className="text-sm text-gray-400 pt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. repellat
-            nrum unde dolor maxime. Vitae voluptatibus rem assumenda saepe
-            consequuntur?
-          </p>
-          <p className="text-sm text-gray-400 pt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel lat
-            nostrum unde dolor maxime. Vitae voluptatibus rem assumenda saepe
-            consequuntur?
-          </p>
-          <p className="text-sm text-gray-400 pt-2">
-            Lorem ipsum dolor sit, amuntur?
+           {props.unordered.unorderedsusubtitle}
           </p>
 
           <div className="container mx-auto pt-6 pb-6">
@@ -159,7 +184,7 @@ function Secondpage() {
 
             <div className="container mx-auto pt-6 pb-6">
               <p className="font-bold">Related Post</p>
-              <div className="grid grid-cols-2">
+              <div className="md:grid grid-cols-2">
                 <div className="col-span-1 pt-6 pb-6">
                   <ImgText />
                 </div>
@@ -170,17 +195,17 @@ function Secondpage() {
             </div>
 
             <div className="container mx-auto pb-6 pt-6">
-              <div className="bg-white p-4">
+              <div className="bg-white p-3">
                 <div className="flex">
                   <div className="flex-shrink pt-6 pr-4">
                     <div className="small3 mt-4"></div>
                   </div>
-                  <div className="flex-shrink ">
+                  <div className="flex-shrink">
                     <p className="text-sm text-gray-400 pt-2">written by</p>
-                    <p className="text-2xl font-bold pt-2">
+                    <p className="text-2xl font-bold pt-1">
                       Rosalina D.William
                     </p>
-                    <p className="text-sm text-gray-400 pt-2">
+                    <p className="text-sm text-gray-400 pt-1">
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                       Deserunt in dolorem magni quae, rem totam impedit quidem!
                       Ipsam, iure saepe?
@@ -205,7 +230,7 @@ function Secondpage() {
           </div>
         </div>
 
-        <div className="pl-12">
+        <div className="pl-24 w-10/12">
           <Socialconnect />
         </div>
       </div>
@@ -217,4 +242,16 @@ function Secondpage() {
   );
 }
 
-export default Secondpage;
+export async function getServerSideProps(context) {
+  const response = await axios.get("http://localhost:3000/api/detailPosts");
+  return {
+    props: {
+      paragraph: response.data.paragraph,
+      hereCome: response.data.hereCome,
+      wrapped: response.data.wrapped,
+      unordered: response.data.unordered,
+
+      // sectionAlone: response.data.sectionAlone,
+    }, // will be passed to the page component as props
+  };
+}
