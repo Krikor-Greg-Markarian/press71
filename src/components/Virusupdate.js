0713d7931bb0807f82680efb0f0b81aa3738bacd
Greg <greg_markarian@hotmail.com>
2021-09-21 17:08:15 +0300

    virud update component

--- src/components/Virusupdate.js
+++ src/components/Virusupdate.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Virusupdate(props) {
   return (
-    <div className="">
-      <div className="flex ">
+    <div className="pl-6">
+      <div className="flex">
         <div className="flex-grow ">
-          <p className="text-gray-400 text-sm pl-6 pb-2">{props.Stories}</p>
+          <p className="text-gray-400 text-sm pl-6 pb-2 ">{props.Stories}</p>
           <p className="pl-6 pr-1 pb-2">{props.adviceCorona}</p>
         </div>
         <div className="flex-shrink pt-2 pl-1">
