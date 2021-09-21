5c0db2b403f07780883be6fc8e5088cc08951831
Greg <greg_markarian@hotmail.com>
2021-09-21 10:47:40 +0300

    virus update section

--- src/components/Virusupdate.js
+++ src/components/Virusupdate.js
@@ -1,25 +1,18 @@
 import React from "react";
 
-function Virusupdate() {
+function Virusupdate(props) {
   return (
-    
-      <div className = "">
-        <div className="flex ">
-          
-          <div className="flex-grow ">
-            <p className="text-gray-400 text-sm pl-6 pb-2">VIRUS UPDATE</p>
-            <p className = "pl-6 pr-1 pb-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-              nobis.
-            </p>
-          </div>
-          <div className="flex-shrink pt-2 pl-1">
-            <div className="small"></div>
-          </div>
+    <div className="">
+      <div className="flex ">
+        <div className="flex-grow ">
+          <p className="text-gray-400 text-sm pl-6 pb-2">{props.Stories}</p>
+          <p className="pl-6 pr-1 pb-2">{props.adviceCorona}</p>
         </div>
+        <div className="flex-shrink pt-2 pl-1">
+          <div className="small"></div>
         </div>
-      
-    
+      </div>
+    </div>
   );
 }
 
