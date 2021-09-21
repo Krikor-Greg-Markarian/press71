b99224e374c79e0979b003bb86152adef70977c4
Greg <greg_markarian@hotmail.com>
2021-09-21 13:07:18 +0300

    latest article component

--- src/components/Latestarticle.js
+++ src/components/Latestarticle.js
@@ -6,7 +6,7 @@ function Latestarticle(props) {
        
           <div className = "sm:mb-4 ">
             <div className="design2 w-full relative">
-              <button className = "text-white bg-red-600 px-2 text-sm absolute bottom-0">DOCTOR</button>
+              <button className = "text-white bg-red-600 px-2 text-sm absolute bottom-0">{props.button}{props.button1}{props.button2}{props.button3}{props.button4}{props.button5}{props.button6}{props.button7}{props.button8}{props.button9}{props.button10}{props.button11}</button>
             </div>
             <p className = "font-bold text-sm pt-4">{props.forteen}{props.title}</p>
             <p className = "text-sm text-gray-400 pt-4 pb-4">
