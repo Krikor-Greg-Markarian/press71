2404852d3c29b695fa4f47dadeb6783cd8a228b8
Greg <greg_markarian@hotmail.com>
2021-09-21 16:40:35 +0300

    ADD: padding fever section

--- src/components/Fever.js
+++ src/components/Fever.js
@@ -3,13 +3,13 @@ import React from "react";
 function Fever(props) {
   return (
     
-      <div className = "">
+      <div className = "pl-6">
         <div className="design w-full"></div>
         <div className = "pt-2">
         <button className="bg-red-600 text-white px-6 ">{props.coronabutton}</button>
         </div>
         <p className = "font-bold pt-2 pb-2">{props.why}</p>
-        <p className = "text-gray-400 text-sm sm:mb-4">{props.asupermarket}</p>
+        <p className = "text-gray-400 text-sm  sm:mb-4 ">{props.asupermarket}</p>
       </div>
     
   );
