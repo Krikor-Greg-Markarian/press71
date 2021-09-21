e16385aec34b17b388af85a913e46c59219473fb
Greg <greg_markarian@hotmail.com>
2021-09-21 12:23:14 +0300

    ADD: JSON 3rd page

--- pages/api/detaillPosts.js
+++ pages/api/detaillPosts.js
@@ -4,6 +4,9 @@ export default function handler(req, res) {
             name:"Rasalina De Willamson",
             articles:"64 Articles Written 2comments",
         },
+        text:{
+            lorem: "Rasalina De has worked a italy report since its launch in 2011. In the years since, he has led by example the company's tech news team and published over 5,000 articles - a mix of breaking news,reviews,helpful how-tos, industry analysis, and more.n Romero has worked a Italy Report since its launch in 2011. In the years since.",
+        },
     };
     res.status(200).json(object);
   }
