bda6eb0fc45307cb74cf4be0d8242f00c7ef3b18
Greg <greg_markarian@hotmail.com>
2021-09-21 10:47:15 +0300

    latest article

--- src/components/Latestarticle.js
+++ src/components/Latestarticle.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Latestarticle() {
+function Latestarticle(props) {
     return (
        
        
@@ -8,10 +8,9 @@ function Latestarticle() {
             <div className="design2 w-full relative">
               <button className = "text-white bg-red-600 px-2 text-sm absolute bottom-0">DOCTOR</button>
             </div>
-            <p className = "font-bold text-sm pt-4">14 coronavirus myths busted by WHO fo better Covid-19 awareness</p>
+            <p className = "font-bold text-sm pt-4">{props.forteen}</p>
             <p className = "text-sm text-gray-400 pt-4 pb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              laborum exercitationem asperiores doloribus impedit.
+              {props.asupermarket2}
             </p>
             <p className="inline font-bold text-sm">By: <span className = "text-blue-500 text-sm inline font-bold">RASALINA</span> </p>
             <p className="inline text-gray-400 text-sm">| 23 comments</p>
