88683afcb0f5934ef37639f800bd4d2cad4b331e
Greg <greg_markarian@hotmail.com>
2021-09-21 10:47:30 +0300

    small advice section

--- src/components/Smalladvice.js
+++ src/components/Smalladvice.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Smalladvice() {
+function Smalladvice(props) {
     return (
         
             <div>
-                <p className = "text-gray-500 text-sm pt-4">STORIES</p>
-                <p className = "text-sm pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet exercitationem facere laborum voluptatum eum itaque.</p>
+                <p className = "text-gray-500 text-sm pt-4">{props.Stories}</p>
+                <p className = "text-sm pt-2">{props.adviceCorona}</p>
                 <p className = "text-gray-500 text-sm pt-2">34 MINUTES AGO </p>
                 <p className = "text-black text-sm inline">By: <span className = "text-blue-600 text-sm inline">RASALINA</span> </p>  
                 
