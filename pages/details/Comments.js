feaa9294da6eb4d62597cc5da3a495652f90882d
Greg <greg_markarian@hotmail.com>
2021-09-21 11:31:02 +0300

    detail post section

--- pages/details/Comments.js
+++ pages/details/Comments.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-function Comments() {
+function Comments(props) {
   return (
     <div>
       <div className="flex pt-4">
         <div className="flex-shrink">
-          <div className="iconprofile rounded-full "></div>
+          <div className="iconprofile rounded-full"></div>
         </div>
         <div className="flex-grow w-20">
-          <p className="font-bold">Rosalina Kelian</p>
-          <p className="text-gray-400">24th March 2019</p>
+          <p className="font-bold pl-2">{props.name}</p>
+          <p className="text-gray-400 pl-2">{props.seconddate}</p>
         </div>
         <div className="flex-grow">
           <button className="border-2 px-4 text-gray-400">Reply</button>
         </div>
       </div>
-      <p className="text-gray-400 text-sm border-b-2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
-        sapiente.
+      <p className="text-gray-400 text-sm border-b-2 pl-14">
+        {props.articlelorem}
       </p>
     </div>
   );
