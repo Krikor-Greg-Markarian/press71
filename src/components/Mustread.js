2867210f422cb0955be8df919f9bc996f8b87102
Greg <greg_markarian@hotmail.com>
2021-09-21 14:48:13 +0300

    ADD: must read

--- src/components/Mustread.js
+++ src/components/Mustread.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function Mustread() {
+function Mustread(props) {
   return (
     <div>
       <div className="pt-2 pb-2">
         <p className="text-gray-400 inline text-sm">
-          24 DECEMBER 2021{" "}
+          {props.date}
           <span className="text-gray-400 inline text-sm">
             <span className="text-black font-bold text-sm pl-4 ">BY :</span> RASALINA
           </span>
         </p>
         <p className="font-bold text-sm pt-2 pb-2">
-          Coronavirus live updates:italy death toll nears 3,000
+          {props.title}
         </p>
         <div className="border-b-2"></div>
       </div>
