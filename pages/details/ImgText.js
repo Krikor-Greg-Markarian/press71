962b1a4fdec61f205128f50756ed57b98aceaea5
Greg <greg_markarian@hotmail.com>
2021-09-21 10:46:42 +0300

    img text section

--- pages/details/ImgText.js
+++ pages/details/ImgText.js
@@ -4,9 +4,9 @@ function ImgText() {
     return (
         <div className = "border-2">
             <div className = "apple"></div>
-            <div className = "p-2 bg-white">
+            <div className = "p-4 bg-white">
                 <p className = "text-gray-400 text-sm">24th March 2019</p>
-                <p className = "font-bold pt-2">Wacth: 10 Days Lter: What italians Wish They Had Known</p>
+                <p className = "font-bold pt-2 text-sm">Wacth: 10 Days Lter: What italians Wish They Had Known</p>
                 <p className = "text-gray-400 text-sm pt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, expedita.</p>
             </div>
         </div>
