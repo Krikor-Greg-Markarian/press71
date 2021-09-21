b34d93654539bdc6d26b15a7ef5c75aebdccec33
Greg <greg_markarian@hotmail.com>
2021-09-21 13:07:07 +0300

    ADD: JSON 3rd page

--- pages/api/detaillPosts.js
+++ pages/api/detaillPosts.js
@@ -7,6 +7,83 @@ export default function handler(req, res) {
         text:{
             lorem: "Rasalina De has worked a italy report since its launch in 2011. In the years since, he has led by example the company's tech news team and published over 5,000 articles - a mix of breaking news,reviews,helpful how-tos, industry analysis, and more.n Romero has worked a Italy Report since its launch in 2011. In the years since.",
         },
+
+        allPost:{
+            box:[
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button:"DOCTOR"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button1:"CHECK POST"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button2:"DOCTOR"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button3:"WORK"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button4:"DOCTOR"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button5:"CHECK POST"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button6:"DOCTOR"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button7:"WORK"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button8:"DOCTOR"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button9:"CHECK POST"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button10:"DOCTOR"
+
+                },
+                {
+                    title:"14 Coronavirus myth busted by WHO fo better covid-19 awareness",
+                    subtitle:"A supermarket woker was apat at by a customer attempting to stockpile pot Noodles while another was told (i hope you get the...",
+                    button11:"WORK"
+
+                },
+            ]
+        }
     };
     res.status(200).json(object);
   }
